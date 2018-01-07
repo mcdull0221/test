c2e0abfad86042bc2ea51921e853753f334db0d8
Mcdull <603561287@qq.com>
2018-01-07 21:17:09 +0800

    Initial commit

---
+++ learnTEST/hanshu.py
@@ -0,0 +1,22 @@
+def make_pizza(*toppings):
+    # 星号让Python创建一个名为toppings 的空元组
+    """概述要制作的比萨"""
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+print("------------------------------------------------------------")
+
+
+def make_pizza(size, *toppings):
+    """概述要制作的比萨"""
+    print("\nMaking a " + str(size) + "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
