e45c14312ccfdf0df8db87e6e22977867c321cc8
Mcdull <603561287@qq.com>
2018-09-30 19:53:32 +0800

    pyhon day by day 爬虫的学习

--- learnTEST/爬虫/spider.py
+++ learnTEST/爬虫/spider.py
@@ -4,7 +4,9 @@ import re
 
 class Spider:
     url = 'https://www.huya.com/g/lol'
-    root_pattern = '<span class="txt">[\s\S]*?</i></span>'      # 非贪婪模式
+    root_pattern = '<span class="txt">([\s\S]*?</i>)</span>'      # 非贪婪模式
+    name_pattern = '<i class="nick" title="([\s\S]*?)">'
+    number_pattern = '<i class="js-num">([\s\S]*?)</i>'
 
     def __fetch_content(self):
         r = request.urlopen(Spider.url)
@@ -13,12 +15,49 @@ class Spider:
         return htmls
 
     def __analysis(self, htmls):
+        # 抓取数据
         root_html = re.findall(Spider.root_pattern, htmls)
-        return root_html
+        # print(root_html[0])
+        anchors = []
+        for html in root_html:
+            name = re.findall(Spider.name_pattern, html)
+            number = re.findall(Spider.number_pattern, html)
+            anchor = {'name': name, 'number': number}
+            anchors.append(anchor)
+        # print(anchors)
+        return anchors
+
+    def __refine(self, anchors):
+        # 精炼数据
+        l = lambda anchor: {
+            'name': anchor['name'][0].strip(),
+            'number': anchor['number'][0]
+        }
+        return map(l, anchors)
+
+    def __sort(self, anchors):
+        # 排序
+        anchors = sorted(anchors, key=self.__sort_seed, reverse=True)
+        return anchors
+
+    def __sort_seed(self, anchor):
+        # 根据人数排序
+        r = re.findall('\d*', anchor['number'])
+        number = float(r[0])
+        if '万' in anchor['number']:
+            number *= 10000
+        return number
+
+    def __show(self, anchors):
+        for rank in range(0, len(anchors)):
+            print('排名 ' + str(rank+1) + ':' + anchors[rank]['name'] + '    ' + anchors[rank]['number'])
 
     def go(self):
         htmls = self.__fetch_content()
-        self.__analysis(htmls)
+        anchors = self.__analysis(htmls)
+        anchors = list(self.__refine(anchors))
+        anchors = self.__sort(anchors)
+        self.__show(anchors)
 
 
 if __name__ == '__main__':
