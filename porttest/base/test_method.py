b47d03fbab7e1eb6328ba29c7923e1f927df1004
Mcdull <603561287@qq.com>
2018-10-28 12:07:44 +0800

    实现mock 模拟返回数据功能

--- porttest/base/test_method.py
+++ porttest/base/test_method.py
@@ -3,6 +3,7 @@ from base.demo import RunMain
 import json
 import HTMLTestRunner
 import os
+from mock import mock
 
 
 class PortTest(unittest.TestCase):
@@ -49,6 +50,27 @@ class PortTest(unittest.TestCase):
         # else:
         #     print('测试失败')
 
+    def test_03(self):
+        """mock 模拟返回的数据"""
+        url = 'http://coding.imooc.com/api/cate'
+        data = {
+            'timestamp': '1507034803124',
+            'uid': '5249191',
+            'uuid': '5ae7d1a22c82fb89c78f603420870ad7',
+            'secrect': '078474b41dd37ddd5efeb04aa591ec12',
+            'token': '7d6f14f21ec96d755de41e6c076758dd',
+            'cid': '0',
+            'errorCode': 1001
+        }
+        # self.run.run_main = mock.Mock(return_value=data)
+        mock_data = mock.Mock(return_value=data)
+        self.run.run_main = mock_data
+        # res = mock_test(self.run.run_main, data, url, "POST", data)
+        res = self.run.run_main(url, 'POST', data)
+        print(res)
+        self.assertEqual(res['errorCode'], 1001, "测试失败")
+
+
 
 if __name__ == '__main__':
     # filepath = "../report/htmlreport.html"
@@ -56,6 +78,7 @@ if __name__ == '__main__':
     suite = unittest.TestSuite()
     suite.addTest(PortTest('test_01'))
     suite.addTest(PortTest('test_02'))
+    suite.addTest(PortTest('test_03'))
     unittest.TextTestRunner().run(suite)
     # runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='test')
     # runner.run(suite)
